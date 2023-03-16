34e9784019279931a0945e923097bacf3f471eb9
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-03-16 08:28:59 +0300

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+//code your solution here
+function saturdayFun(x) {
+  if (x === undefined) {
+    return "This Saturday, I want to roller-skate!";
+  } else {
+    return "This Saturday, I want to bathe my dog!";
+  }
+}
+
+function mondayWork(x) {
+  if (x === undefined) {
+    return "This Monday, I will go to the office.";
+  } else {
+    return "This Monday, I will work from home.";
+  }
+}
+
+function wrapAdjective(string) {
+  function inner(special) {
+    if (string === "*") {
+      return `You are *${special}*!`;
+    } else {
+      return `You are ||${special}||!`;
+    }
+  }
+  return inner;
+}
